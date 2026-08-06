84bd939ab66f51a9f76681ae87c86c63e9d758ab
Empirehelp <officialempirehubhelp@gmail.com>
2026-08-06 13:10:07 +0500

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_pwd(password):
-    return generate_password_hash(password)
-
-def verify_pwd(stored_hash, provided_password):
-    return check_password_hash(stored_hash, provided_password) or stored_hash == provided_password
